0b9c5c4fb48c79f71bc849be89a2425f13eee796
sauravp99 <saurav.pa99@gmail.com>
2025-10-07 23:36:17 +0200

    refactor

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,57 +1,46 @@
 "use client";
+
 import Image from "next/image";
-import {IKeyword, KeywordText} from "./(home)/Keyword";
 import { useState } from "react";
 import Link from "next/link";
 
-
 export default function Home() {
-
-	const [activeKeyword, setActiveKeyword] = useState<IKeyword>({
-		type: "default",
-		action: "hover"
-	  });
-	  
-	  const getBackgroundClass = () => {
-		console.log("Getting background class for ", activeKeyword.type);
-		switch (activeKeyword.type) {
-		  case "code": return "bg-red-900";
-		  case "design": return "bg-blue-900";
-		  case "art": return "bg-green-900";
-		  case "default":
-		  default: return "bg-[var(--primary)]";
-		}
-	  };
-	  
+  const [isKeywordsHighlighted, setIsKeywordsHighlighted] = useState(false);	
+	
   return (
-    <div
-      className={`flex flex-col h-full justify-between bg-[var(--primary)] px-8 lg:px-32 transition-colors duration-300`}
-    >
-      <main className="flex flex-col gap-16 h-full text-center justify-center items-center sm:text-left font-[family-name:var(--font-cherry-bomb-one)]">
-        <p className="lg:text-5xl text-3xl">Hi! I am Saurav</p>
-        <div className="relative text-2xl lg:text-4xl lg:w-96 lg:h-96 w-[300px] h-[300px] overflow-hidden">
-          <Image
-            aria-hidden
-            src="/avatar_hello.svg"
-            alt="Avatar illustration"
-            className={`absolute inset-0`}
-            width={300}
-            height={300}
-            priority
-          />
-        </div>
-        <p className="cursor-default">
+    <main className="flex flex-col bg-[var(--primary)] px-8 lg:px-32 min-h-[calc(100vh-48px)] justify-between py-8">
+      <div className="flex flex-col items-center justify-center flex-grow gap-6 md:gap-8">
+        <p className="lg:text-5xl text-4xl font-[family-name:var(--font-cherry-bomb-one)]">Hi! I am Saurav</p>
+        <Image
+          aria-hidden
+          src="/avatar_hello.svg"
+          alt="Avatar illustration"
+          className="lg:w-[400px] lg:h-[400px] w-[300px] h-[300px]"
+          width={300}
+          height={300}
+          priority
+        />
+        <p className="cursor-default [&_span]:transition-colors lg:text-5xl text-3xl [&_span]:duration-300 font-[family-name:var(--font-cherry-bomb-one)]">
           I love to{" "}
-          <KeywordText keywordType="code" setActiveKeyword={setActiveKeyword}/>{", "}
-          <KeywordText keywordType="design" setActiveKeyword={setActiveKeyword}/>{", "}
+          <span className={`hover:text-red-300 ${isKeywordsHighlighted ? "text-red-300" : ""}`}>Code</span>{", "}
+          <span className={`hover:text-blue-300 ${isKeywordsHighlighted ? "text-blue-300" : ""}`}>Design</span>{", "}
           and make{" "}
-          <KeywordText keywordType="art" setActiveKeyword={setActiveKeyword}/>{"!"}
+          <span className={`hover:text-green-300 ${isKeywordsHighlighted ? "text-green-300" : ""}`}>Art</span>{"!"}
         </p>
-		<p className="text-2xl">
-		welcome to my portfolio
-		</p>
-		<Link href="/code">{"->"}</Link>
-      </main>
-    </div>
+      </div>
+      <div className="flex flex-col items-center mt-6">
+        <p className="text-2xl mb-2 font-[family-name:var(--font-cherry-bomb-one)]">
+          welcome to my portfolio
+        </p>
+        <Link href="/code" className="text-4xl transition-transform hover:translate-x-2">
+          <span 
+            onMouseEnter={() => setIsKeywordsHighlighted(true)} 
+            onMouseLeave={() => setIsKeywordsHighlighted(false)}
+          >
+            {"->"}
+          </span>
+        </Link>
+      </div>
+    </main>
   );
 }
